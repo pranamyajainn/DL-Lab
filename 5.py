7de24096f23c1abaae876e121ff81235e8439bd3
Pranamya Jain <146780116+pranamyajainn@users.noreply.github.com>
2025-06-26 21:11:58 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,23 +1,31 @@
-#Q5 Implement image style transfer, transforming a given content image to adopt the artistic style of another image,
-#using a pre-trained model.
+# Q5: Implement image style transfer using a pre-trained model (one-by-one image display)
+
 import tensorflow as tf
 import tensorflow_hub as hub
 import matplotlib.pyplot as plt
 
+# Load and preprocess the image
 def load_and_process_image(image_path):
-    img = tf.image.resize(tf.image.decode_image(tf.io.read_file(image_path), channels=3), [512, 512])
-    return img[tf.newaxis, ...] / 255.0
+    img = tf.image.decode_image(tf.io.read_file(image_path), channels=3)
+    img = tf.image.resize(img, [512, 512])
+    return img[tf.newaxis, ...] / 255.0  # normalize and add batch dimension
 
-model = hub.load('model')
+# Load pre-trained model from TensorFlow Hub
+model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 
+# Load content and style images
 content_image = load_and_process_image('content.jpeg')
 style_image = load_and_process_image('style.jpeg')
 
+# Apply style transfer
 stylized_image = model(content_image, style_image)[0]
 
-plt.figure(figsize=(12, 4))
-for i, img in enumerate([content_image, style_image, stylized_image], 1):
-    plt.subplot(1, 3, i)
-    plt.imshow(img[0])
+# Show images one by one with titles
+titles = ['Content Image', 'Style Image', 'Stylized Image']
+images = [content_image, style_image, stylized_image]
+
+for i in range(3):
+    plt.imshow(images[i][0])
+    plt.title(titles[i])
     plt.axis('off')
-plt.show()
+    plt.show()
